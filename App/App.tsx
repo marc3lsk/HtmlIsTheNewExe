638ecafdba8c9ab460f8e06922457aae93fa9d59
marc3lsk <marc3lsk@gmail.com>
2019-11-20 16:08:04 +0100

    $Content $Footer

--- App/App.tsx
+++ App/App.tsx
@@ -1,17 +1,17 @@
 import * as m from "mithril";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { getState, setState } from "./State";
-import content from "./Content";
-import footer from "./Footer";
+import $Content from "./Content";
+import $Footer from "./Footer";
 
 function redraw(init = false) {
     m.render(document.body, [
-        <div class="flex-grow-1">{content({ redraw, getState, setState, init })}</div>,
+        <div class="flex-grow-1">{$Content({ redraw, getState, setState, init })}</div>,
         <footer
             class="d-block bg-light p-3"
             oncreate={vnode => {
                 var redraw = function(init = false) {
-                    m.render(vnode.dom, footer({ redraw, getState, setState, init }));
+                    m.render(vnode.dom, $Footer({ redraw, getState, setState, init }));
                 };
                 redraw(init);
             }}></footer>
